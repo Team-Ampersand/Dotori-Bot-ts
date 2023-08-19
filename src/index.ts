1b5437cef88b44e65e21a74dc4ec25d9026969ea
baegteun <baegteun@gmail.com>
2023-08-19 17:58:43 +0900

    :sparkles: :: bot start

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
+import { DotoriBot } from "./DotoriBot";
 
+export const bot = new DotoriBot(
+    new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+		    GatewayIntentBits.GuildMessages,
+		    GatewayIntentBits.MessageContent,
+		    GatewayIntentBits.GuildMembers
+        ]
+    })
+);
